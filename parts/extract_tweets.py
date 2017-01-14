6afdc2d53cf2e2a57290bdff810a13c7dbe248bf
Siddharth <sgiedne@gmail.com>
2017-01-14 17:26:54 -0600

    First Commit | Extract Tweets | Sidd | 14 Jan 2017

---
+++ parts/extract_tweets.py
@@ -0,0 +1,9 @@
+tweets_data_path = './goldenglobes.tab'
+
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for tweet in tweets_file:
+    try:
+        tweets_data.append(tweet)
+    except:
+        continue
