d877daabeb006f0c773bf54eb7df9dae00bd9149
Eureka Foong <eureka.foong@gmail.com>
2017-02-02 23:27:05 -0600

    Used re.split to split presenter-related tweets and NE chunk to find potential presenter names

--- parts/find_presenter.py
+++ parts/find_presenter.py
@@ -34,27 +34,43 @@ chunked_data = []
 presenter_award_data = []
 i=0
 
-#Extracts poeple(potential presenters) and organizations/GPEs(potential award names) within every tweet present in tweets_presenter_data. Resulting list in stored in presenter_award_data.
+potential_presenters = []
+#This looks at each tweet (document) in the tweets_presenter_data (corpus)
 for tweet in tweets_presenter_data:
-    chunked_data = ne_chunk(pos_tag(word_tokenize(tweet)))
-    i=0
-    #print ''
-    n1 = []
+    try: 
+        #Splits the tweet right before it mentions presenting/presents/presented the award 
+        split_tweet = re.split(' present.* Best ', tweet)
+        
+        #With the split tweet, look only at the first split
+        #Look from the end of the first split. If we hit a name, take that as the name
+        #First, we need to tokenize each word
+        tagged_words = ne_chunk(pos_tag(word_tokenize(split_tweet[0])))
+        
+        #Start from the end of the first split
+        i = len(tagged_words) - 1
+
+        print tweet
+        while i >= 0:
+            if hasattr(tagged_words[i], 'label'):
+                #Do not consider any "GoldenGlobes"
+                if "Golden" not in str(tagged_words[i]):
+                    #potential_presenters.append(list(tagged_words[i])) (ADD THIS LATER)
+                    #Print anything in the first split that has a label that is not GoldenGlobes
+                    #PROBLEM: Misses anything with "@" in front of it
+                    #PROBLEM: How to deal with retweet RT "@"s?
+                    print tagged_words[i]
+            i=i-1
+
+        #ALTERNATIVE 1: We could calculate TFIDF and take the name as the one with the highest TFIDF
+        #ALTERNATIVE 2: We could also look for "and" in tagged_words and see if there are pronouns around them 
+        
+        #NEXT: Now look at the second split and see if you can find the presenter there 
+        #This will help us determine when the presenter might be mentioned in the second half of the tweet
+    except:
+        continue
+
+#Testing structure of tweets with "Golden Globes" before " present"
+split_tweet1 = re.split(' present.* Best ', tweets_presenter_data[6])
+print split_tweet1
+#print potential_presenters
     
-    while(i<len(chunked_data)):
-        str_data = str(chunked_data[i])
-        #Considers only the chunked data before the first mention of "Best"
-        if "Best" not in str_data:
-            #Detects variations of Golden Globes in the chunked data (DOESN'T QUITE WORK YET!)
-            if (hasattr(chunked_data[i],'label') and not (("Golden" in str_data) or ("Globe" in str_data))):
-                n1.append(str_data)
-            i+=1
-        else:
-            break
-
-        presenter_award_data.append(n1)
-        chunked_data = []
-
-for i in presenter_award_data:
-    print i
-    print ''
